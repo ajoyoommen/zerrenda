74e5a3e347fee91993604dd8407c17fe05da346b
Ajoy Oommen <ajoy91ad@gmail.com>
2016-06-26 15:10:41 +0530

    Remove duplicate INSTALLED_APPS from settings

--- zerrenda/settings/development.py
+++ zerrenda/settings/development.py
@@ -1,12 +1,3 @@
 from zerrenda.settings import *
 
 DEBUG = True
-
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles'
-)
