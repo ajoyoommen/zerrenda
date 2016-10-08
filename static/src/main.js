55dab90a4e9ade7872c3d1aafde9b16525957c43
Ajoy Oommen <ajoy91ad@gmail.com>
2016-10-08 23:13:15 +0530

    Render static content with React

--- static/src/main.js
+++ static/src/main.js
@@ -1,4 +1,39 @@
+var Item = React.createClass({
+  render: function () {
+    return (
+      <a href="#" className="list-group-item">
+        <h4 className="list-group-item-heading">{this.props.name}</h4>
+      </a>
+    )
+  }
+});
+
+var List = React.createClass({
+  render: function () {
+    return (
+      <div className="col-md-3">
+        <div className="list-group">
+          <Item name="do this first"/>
+          <Item name="this next"/>
+        </div>
+      </div>
+    )
+  }
+});
+
+var ToDoComponent = React.createClass({
+  render: function () {
+    return (
+      <div className="row">
+        <h3>All your lists</h3>
+        <List name="main list"/>
+        <List name="no idea"/>
+      </div>
+    )
+  }
+});
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('example')
+  <ToDoComponent />,
+  document.getElementById('react-app')
 );
--- templates/index.tpl
+++ templates/index.tpl
@@ -3,12 +3,18 @@
   <head>
     <meta charset="UTF-8" />
     <title>Zerrenda</title>
+
+    <link rel="stylesheet" href="/static/libs/bootstrap.min.css">
     <script src="/static/libs/react.js"></script>
     <script src="/static/libs/react-dom.js"></script>
     <script src="/static/libs/browser.min.js"></script>
   </head>
   <body>
-    <div id="example"></div>
+    <div class="container">
+      <h1 class="text-center">ToDo app with Zerrenda</h1>
+
+      <div id="react-app"></div>
+    </div>
     <script type="text/babel" src="/static/src/main.js"></script>
   </body>
 </html>
