9262ca3918c47df268d857effdce0079edcb9f12
Ajoy Oommen <ajoy91ad@gmail.com>
2016-10-08 13:34:16 +0530

    Add SafeDeleteMixin

--- common/models.py
+++ common/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 
-class TimeStampedModel(models.Model):
+class TimeStampedMixin(models.Model):
     created = models.DateTimeField(editable=False)
     modified = models.DateTimeField(editable=False)
 
@@ -14,3 +14,27 @@ class TimeStampedModel(models.Model):
             self.created = timezone.now()
         self.modified = timezone.now()
         super(TimeStampedModel, self).save(*args, **kwargs)
+
+
+class SoftDeleteMixin(models.Model):
+    delete = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+    def delete(self, force=False):
+        if force:
+            super(SoftDeleteMixin, self).delete()
+        else:
+            self.delete = True
+            self.save()
+
+    def save(self, undelete=False, *args, **kwargs):
+        if undelete:
+            self.delete = False
+        super(SoftDeleteMixin, self).save(*args, **kwargs)
+
+
+class DeleteSafeTimeStampedMixin(SoftDeleteMixin, TimeStampedMixin):
+    class Meta:
+        abstract = True
