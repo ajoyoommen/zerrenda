ba23acec8b70d9731c846c757c70e4c7301338a7
Ajoy Oommen <ajoy91ad@gmail.com>
2016-10-09 18:04:09 +0530

    Render components dynamically from a list

--- static/src/main.js
+++ static/src/main.js
@@ -2,7 +2,7 @@ var Item = React.createClass({
   render: function () {
     return (
       <a href="#" className="list-group-item">
-        <h4 className="list-group-item-heading">{this.props.name}</h4>
+        <p className="list-group-item-text">{this.props.name}</p>
       </a>
     )
   }
@@ -10,11 +10,17 @@ var Item = React.createClass({
 
 var List = React.createClass({
   render: function () {
+    var items = this.props.items.map(function (item) {
+      return (
+        <Item key={item.id} name={item.name}/>
+      );
+    });
+
     return (
       <div className="col-md-3">
         <div className="list-group">
-          <Item name="do this first"/>
-          <Item name="this next"/>
+          <h4>{this.props.name || 'Untitled List'}</h4>
+          {items}
         </div>
       </div>
     )
@@ -23,11 +29,27 @@ var List = React.createClass({
 
 var ToDoComponent = React.createClass({
   render: function () {
+    var list_a = [
+      {id: 1, name: 'learn to code', items: [
+        {id: 2, name: 'learn syntax'},
+        {id: 3, name: 'learn why it works'}
+      ]},
+      {id: 4, name: 'learn to debug', items:[
+        {id:5, name: 'find the approximate location'},
+        {id:6, name: 'follow the error trail'}
+      ]}
+    ];
+
+    var lists = list_a.map(function (list) {
+      return (
+        <List key={list.id} name={list.name} items={list.items} />
+      );
+    });
+
     return (
       <div className="row">
         <h3>All your lists</h3>
-        <List name="main list"/>
-        <List name="no idea"/>
+        {lists}
       </div>
     )
   }
