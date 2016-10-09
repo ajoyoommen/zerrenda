47df49dee62a24b430202b80f4fc997815bef001
Ajoy Oommen <ajoy91ad@gmail.com>
2016-10-09 23:03:10 +0530

    Fix errors

--- zerrenda/urls.py
+++ zerrenda/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api-auth/', include('rest_framework.urls')),
+    url(r'api/', include('api.urls', namespace='api')),
     url(r'', include('website.urls', namespace='website')),
 ]
